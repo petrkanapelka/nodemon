847fb346485fd97fd7c243fba060a4a839f7bf59
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-12 20:48:18 +0300

    feat: Implement API endpoints for product management
    Added REST API endpoints for managing products, including retrieving all products, filtering products by title, creating new products, retrieving a single product by ID, and deleting a product by ID.  Included UUIDs for product and address resources and added filtering by title for products.  Also updated dependencies to support new functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,71 @@
 import express, { Request, Response } from 'express';
+import { v4 } from 'uuid';
 
 // create express app
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-const products = [{ title: 'tomato' }, { title: 'orange' }];
-const addresses = [{ value: 'Nezalejnasti 12' }, { value: 'Selickaga 11' }];
+let products = [
+    { id: '1', title: 'tomato' },
+    { id: '2', title: 'orange' },
+];
+let addresses = [
+    { id: '1', value: 'Nezalejnasti 12' },
+    { id: '2', value: 'Selickaga 11' },
+];
+
+app.use(express.json());
 
 app.get('/products', (req: Request, res: Response) => {
-    res.send(products);
+    if (req.query.title) {
+        const title = req.query.title.toString();
+        res.send(products.filter((p) => p.title.includes(title)));
+    } else {
+        res.send(products);
+    }
 });
 
-app.get('/products:productSearchTitle', (req: Request, res: Response) => {
-    const productSearchTitle = req.params.productSearchTitle;
-    let product = products.find((product) => product.title === productSearchTitle);
-    res.send(product);
+app.post('/products', (req: Request, res: Response) => {
+    const newProduct = { id: v4(), title: req.body.title };
+    products.push(newProduct);
+    res.status(201).send(newProduct);
+});
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id;
+    let product = products.find((product) => product.id === id);
+    if (product) {
+        res.send(product);
+    } else {
+        res.send(404);
+    }
+});
+
+app.delete('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id;
+    const product = products.find((p) => p.id === id);
+    if (product) {
+        products = products.filter((product) => product.id !== id);
+        res.send(products);
+        res.send(204);
+    } else {
+        res.send(404);
+    }
 });
 
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses);
+});
+
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    const id = req.params.id;
+    let address = addresses.find((address) => address.id === id);
+    if (address) {
+        res.send(address);
+    } else {
+        res.send(404);
+    }
 });
 
 // start app
