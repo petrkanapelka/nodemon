7d0884e565f48da8e907106710ca96c6b152df1e
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-12 19:26:06 +0300

    Implement basic product and address endpoints
    Added API endpoints for retrieving product and address data.  This lays the groundwork for future features requiring access to this information.  The server port is also now configurable via environment variable or defaults to 5000.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response } from 'express';
 
+// create express app
 const app = express();
-const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Petro!');
+const port = process.env.PORT || 5000;
+
+const products = [{ title: 'tomato' }, { title: 'orange' }];
+const addresses = [{ value: 'Nezalejnasti 12' }, { value: 'Selickaga 11' }];
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products);
 });
 
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses);
+});
+
+// start app
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port: ${port}`);
 });
