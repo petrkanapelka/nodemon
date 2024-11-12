bccf473aa6ab9dc72157565a421774326a210a9f
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-12 19:32:36 +0300

    feat: Implement product search endpoint
    Added a new endpoint to search for products by their title.  This allows clients to retrieve specific product information directly.

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,12 @@ app.get('/products', (req: Request, res: Response) => {
     res.send(products);
 });
 
+app.get('/products:productSearchTitle', (req: Request, res: Response) => {
+    const productSearchTitle = req.params.productSearchTitle;
+    let product = products.find((product) => product.title === productSearchTitle);
+    res.send(product);
+});
+
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses);
 });
