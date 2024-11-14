596cb732eaf4bb8ce226109ebf37ea4fc7e9ff11
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-15 01:14:35 +0300

    Ensure DB connection before server start
    Initialize the database connection before starting the server to prevent potential issues related to database access after the server is running.  This ensures all routes and middleware function correctly.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import { productsRouter } from './routes/product-router';
 import { addressesRouter } from './routes/addresses-router';
 import { countRequestMiddleware, requestCount } from './middleware/request-counter';
+import { runDb } from './repositories/db';
 
 // Create Express app
 export const app = express();
@@ -16,7 +17,13 @@ app.get('/counter', (req: Request, res: Response) => {
     res.json({ requestCount });
 });
 
+const startApp = async () => {
+    await runDb();
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+    });
+};
+
+startApp();
+
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
