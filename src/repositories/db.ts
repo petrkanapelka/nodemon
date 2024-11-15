68b078e52461a9429c1e573df7a40342a3f43fc3
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-15 15:27:16 +0300

    Refactor: Define ProductType in db module
    Moves the ProductType definition into the db module to centralize data types related to database operations and improve code organization.

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,12 +1,15 @@
 import { MongoClient } from 'mongodb';
-import { ProductType } from './products-repository-db';
+
+export type ProductType = {
+    id: string;
+    title: string;
+};
 
 const mongoUri = process.env.mongoURI || 'mongodb://0.0.0.0:27017/';
 
 export const client = new MongoClient(mongoUri);
 
 export const collection = client.db('shop').collection<ProductType>('products');
-
 
 export async function runDb() {
     try {
